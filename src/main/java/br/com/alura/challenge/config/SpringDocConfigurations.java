747d36a2bc56221e0761ca0570cccfacb5daecd5
Emerson-Freitas <emerson_freitas@outlook.com.br>
2023-03-03 11:28:42 -0300

    Ajustando o método customOpenAPI

--- src/main/java/br/com/alura/challenge/config/SpringDocConfigurations.java
+++ src/main/java/br/com/alura/challenge/config/SpringDocConfigurations.java
@@ -2,6 +2,9 @@ package br.com.alura.challenge.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +17,18 @@ public class SpringDocConfigurations {
         return new OpenAPI()
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
-                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .info(new Info()
+                        .title("API Vídeos")
+                        .description("API Rest, contendo as funcionalidades de CRUD de vídeos e de categorias, além de muitos outros end-points")
+                        .contact(new Contact()
+                                .name("Emerson Freitas de Sousa")
+                                .email("emerson_freitas@outlook.com.br"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://alura-challenge/api/licenca")));
     }
 }
